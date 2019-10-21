3c9e55ec9c9fa00174fcfe1c6231a5920228aa6d
jkuusist <jkuusist@student.hive.fi>
2019-10-21 16:50:42 +0300

    ft_strcmp.c done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 11:19:32 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/10/21 16:26:29 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/10/21 16:43:56 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
-char *ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		strcmp(const char *s1, const char *s2);
 
 #endif
