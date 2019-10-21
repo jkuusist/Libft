700a22011dfa3f724dba0de719487d4deafe9917
jkuusist <jkuusist@student.hive.fi>
2019-10-21 17:02:43 +0300

    ft_strncmp done, plus fixed ft_strcmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,21 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/21 16:43:05 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/10/21 17:00:44 by jkuusist         ###   ########.fr       */
+/*   Created: 2019/10/21 16:52:21 by jkuusist          #+#    #+#             */
+/*   Updated: 2019/10/21 17:01:21 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strcmp(const char *s1, const char *s2)
+#include <string.h>
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int i;
 
 	i = 0;
-	while (s1[i])
+	while ((s1[i]) && (i < (int)n))
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
