7c91be36b6221a6ab78bce9a64a0685f5d25b3d9
jkuusist <jkuusist@student.hive.fi>
2019-10-22 11:30:22 +0300

    ft_memcpy done

---
+++ ft_memcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/22 10:53:11 by jkuusist          #+#    #+#             */
+/*   Updated: 2019/10/22 11:23:00 by jkuusist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char*)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
