5d220b6ca10eb277db4fb2e3b0317f6a06b4cd99
jkuusist <jkuusist@student.hive.fi>
2019-10-23 11:27:22 +0300

    ft_isalpha done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 11:19:32 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/10/22 15:59:02 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/10/23 10:47:50 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *str);
+int		ft_isalpha(int c);
 
 #endif
