80eae280f5522c9eb87b94313d9a7d115fa7813e
jkuusist <jkuusist@student.hive.fi>
2019-10-25 14:16:21 +0300

    ft_putstr_fd done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 11:19:32 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/10/25 14:04:29 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/10/25 14:09:36 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ void	ft_putstr(char const *s);
 void	ft_putendl(char const *s);
 void	ft_putnbr(int n);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char const *s, int fd);
 
 #endif
