702146c6270805345498882f87627bf51753fc5e
jkuusist <jkuusist@student.hive.fi>
2019-10-30 14:56:58 +0200

    added ft_memcpy handling for null arguments

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 10:53:11 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/10/22 11:23:00 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/10/30 14:56:21 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t i;
 
 	i = 0;
+	if (!n || !src || !dst)
+		return (0);
+	if (src == dst)
+		return (dst);
 	while (i < n)
 	{
 		((unsigned char*)dst)[i] = ((unsigned char *)src)[i];
