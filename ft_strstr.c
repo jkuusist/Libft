d535ba2d96a48db62538159b8a88960c26d6e967
jkuusist <jkuusist@student.hive.fi>
2019-10-30 16:47:04 +0200

    fixed ft_strstr to handle needle overlapping with partial needle

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 14:46:53 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/10/29 15:26:22 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/10/30 16:45:35 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strstr(const char *haystack, const char *needle)
 		if (haystack[i] == needle[0])
 		{
 			j = 1;
-			while (haystack[i + j] == needle[j])
+			while ((haystack[i + j] == needle[j]) && (needle[j] != '\0'))
 				j++;
 			if (needle[j] == '\0')
 				return ((char*)&haystack[i]);
