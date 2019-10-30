ae0376a3beb977e73252ce6a39a021a23dcf9060
jkuusist <jkuusist@student.hive.fi>
2019-10-30 10:37:02 +0200

    tweaked ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 16:52:21 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/10/25 11:06:58 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/10/30 10:26:29 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int i;
+	size_t	i;
 
 	i = 0;
-	while ((s1[i]) && (i < (int)n))
+	while ((s1[i]) && (s2[i]) && (i < (n - 1)))
 	{
 		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+			return (((unsigned char)s1[i]) - ((unsigned char)s2[i]));
 		i++;
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (((unsigned char)s1[i]) - ((unsigned char)s2[i]));
 }
