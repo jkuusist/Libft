445ed07ce1b4d93f4690f66426dfe64070e686bb
jkuusist <jkuusist@student.hive.fi>
2019-10-31 16:02:48 +0200

    fixed ft_memcpy to handle null arguments properly

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 10:53:11 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/10/30 14:56:21 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/10/31 11:08:19 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t i;
 
 	i = 0;
-	if (!n || !src || !dst)
-		return (0);
+	if (!n || (src == dst))
+		return (dst);
 	if (src == dst)
 		return (dst);
 	while (i < n)
