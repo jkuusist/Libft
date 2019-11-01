f66da298cf130d66ae7a41a81467985ea48f6c54
jkuusist <jkuusist@student.hive.fi>
2019-11-01 13:58:42 +0200

    added handling for n == 0 in ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 16:52:21 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/10/30 10:26:29 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/01 13:52:10 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (n == 0)
+		return (0);
 	while ((s1[i]) && (s2[i]) && (i < (n - 1)))
 	{
 		if (s1[i] != s2[i])
